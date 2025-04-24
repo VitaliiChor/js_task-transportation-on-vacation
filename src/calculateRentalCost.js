896dd61dba6b4af3b181c075b28495e10a8d8c06
Vitalii Chornyi <vitaliychor@gmail.com>
2025-04-24 11:55:57 +0200

    HW Transportation on Vacation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,16 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const rentPerOneDay = 40;
+  const totalRentPrice = days * rentPerOneDay;
+
+  if (days >= 7) {
+    return totalRentPrice - 50;
+  } else if (days >= 3 && days < 7) {
+    return totalRentPrice - 20;
+  } else {
+    return totalRentPrice;
+  }
 }
 
 module.exports = calculateRentalCost;
